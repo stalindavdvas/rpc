f17581e671867a93e3331398c19355ce37ad2078
Stalin Vasco <150808453+stalindavdvas@users.noreply.github.com>
2024-12-10 09:25:57 -0500

    fix: :bug: add template folder

--- client.py
+++ client.py
@@ -1,12 +1,22 @@
+from flask import Flask, render_template
 import grpc
 import name_pb2
 import name_pb2_grpc
 
-def run():
+app = Flask(__name__)
+
+# Cliente gRPC que obtiene el nombre desde el servidor gRPC
+def get_name_from_grpc():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = name_pb2_grpc.NameServiceStub(channel)
         response = stub.GetName(name_pb2.Empty())
-        print("Nombre recibido: " + response.name)
+        return response.name
+
+@app.route('/')
+def index():
+    # Obtiene el nombre desde el servidor gRPC
+    name = get_name_from_grpc()
+    return render_template('index.html', name=name)
 
 if __name__ == '__main__':
-    run()
+    app.run(debug=True)
